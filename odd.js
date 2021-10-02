1c3bca6c30fac9a7be773c3469b7238145f17faa
Taha Helwani <telhe008@uottawa.ca>
2021-10-02 01:01:41 +0000

    Assignmnet - isOdd function checker

---
+++ odd.js
@@ -0,0 +1,6 @@
+// This function returns if the passed number num is odd or not
+function isOdd(num){
+  return num % 3 === 0;
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
